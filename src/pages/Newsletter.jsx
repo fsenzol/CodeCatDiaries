953c64ed6cfc89bb65d50e62bfb9c3cc5c7b2003
fsenzol <tazuhf@gmail.com>
2024-12-21 23:34:07 +0100

    alot of changes and refining v7

--- src/pages/Newsletter.jsx
+++ src/pages/Newsletter.jsx
@@ -21,7 +21,7 @@ const Newsletter = () => {
 		<section>
 			<div className='flex justify-center items-center w-full flex-col gap-10'>
 				<h1 className='text-lg text-primary'>Newsletters</h1>
-				<h3 className='font-kanit text-7xl'>Stories and interviews</h3>
+				<h3 className='font-kanit text-7xl max-sm:text-2xl'>Stories and interviews</h3>
 				<p className='w-2/3 text-center font-kanit text-xl'>Subscribe to learn about new product features, the latest in technology, solutions and updates.</p>
 				<EmailBar />
 				<p className='font-montserrat font-semibold'>We care about your data!</p>
@@ -34,7 +34,7 @@ const Newsletter = () => {
 			{
 				recentPosts.length > 0 ? (
 					<>
-						<div className="grid w-full grid-cols-3 gap-3 max-md:grid-cols-1 max-md:grid-rows-1 px-10 min-h-80 mb-16">
+						<div className="grid w-full grid-cols-3 gap-3 max-md:grid-cols-1 max-md:grid-rows-1 px-10 max-md:px-4 min-h-80 mb-16">
 
 							{recentPosts.slice(0, 6).map((data, i) => (
 								<div key={i}>
